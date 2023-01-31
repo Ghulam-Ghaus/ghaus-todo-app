3d9abadfcea248448d2e03cfb83536285f2cac32
Ghulam Ghaus <44553882+ghaus266@users.noreply.github.com>
2023-01-31 15:01:40 +0500

    .next file added

---
+++ .next/server/app/page.js
@@ -0,0 +1,624 @@
+(() => {
+var exports = {};
+exports.id = 931;
+exports.ids = [931];
+exports.modules = {
+
+/***/ 8038:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/compiled/react");
+
+/***/ }),
+
+/***/ 8704:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/compiled/react-dom/server-rendering-stub");
+
+/***/ }),
+
+/***/ 7897:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/compiled/react-server-dom-webpack/client");
+
+/***/ }),
+
+/***/ 6786:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/compiled/react/jsx-runtime");
+
+/***/ }),
+
+/***/ 3918:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/amp-context.js");
+
+/***/ }),
+
+/***/ 5732:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/amp-mode.js");
+
+/***/ }),
+
+/***/ 3280:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/app-router-context.js");
+
+/***/ }),
+
+/***/ 2796:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/head-manager-context.js");
+
+/***/ }),
+
+/***/ 9274:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/hooks-client-context.js");
+
+/***/ }),
+
+/***/ 4014:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/i18n/normalize-locale-path.js");
+
+/***/ }),
+
+/***/ 4486:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/image-blur-svg.js");
+
+/***/ }),
+
+/***/ 744:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/image-config-context.js");
+
+/***/ }),
+
+/***/ 5843:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/image-config.js");
+
+/***/ }),
+
+/***/ 9552:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/image-loader");
+
+/***/ }),
+
+/***/ 8524:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/is-plain-object.js");
+
+/***/ }),
+
+/***/ 8020:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/mitt.js");
+
+/***/ }),
+
+/***/ 7342:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/no-ssr-error.js");
+
+/***/ }),
+
+/***/ 4406:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/page-path/denormalize-page-path.js");
+
+/***/ }),
+
+/***/ 4964:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router-context.js");
+
+/***/ }),
+
+/***/ 1751:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/add-path-prefix.js");
+
+/***/ }),
+
+/***/ 6220:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/compare-states.js");
+
+/***/ }),
+
+/***/ 299:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/format-next-pathname-info.js");
+
+/***/ }),
+
+/***/ 3938:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/format-url.js");
+
+/***/ }),
+
+/***/ 9565:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/get-asset-path-from-route.js");
+
+/***/ }),
+
+/***/ 5789:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/get-next-pathname-info.js");
+
+/***/ }),
+
+/***/ 1897:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/is-bot.js");
+
+/***/ }),
+
+/***/ 1428:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/is-dynamic.js");
+
+/***/ }),
+
+/***/ 8854:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/parse-path.js");
+
+/***/ }),
+
+/***/ 1292:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/parse-relative-url.js");
+
+/***/ }),
+
+/***/ 4567:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/path-has-prefix.js");
+
+/***/ }),
+
+/***/ 979:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/querystring.js");
+
+/***/ }),
+
+/***/ 3297:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/remove-trailing-slash.js");
+
+/***/ }),
+
+/***/ 6052:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/resolve-rewrites.js");
+
+/***/ }),
+
+/***/ 4226:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/route-matcher.js");
+
+/***/ }),
+
+/***/ 5052:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/route-regex.js");
+
+/***/ }),
+
+/***/ 3349:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/server-inserted-html.js");
+
+/***/ }),
+
+/***/ 2470:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/side-effect.js");
+
+/***/ }),
+
+/***/ 9232:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/utils.js");
+
+/***/ }),
+
+/***/ 618:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/utils/warn-once.js");
+
+/***/ }),
+
+/***/ 5640:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "AppRouter": () => (/* reexport default from dynamic */ next_dist_client_components_app_router__WEBPACK_IMPORTED_MODULE_0___default.a),
+/* harmony export */   "GlobalError": () => (/* reexport default from dynamic */ next_dist_client_components_error_boundary__WEBPACK_IMPORTED_MODULE_3___default.a),
+/* harmony export */   "LayoutRouter": () => (/* reexport default from dynamic */ next_dist_client_components_layout_router__WEBPACK_IMPORTED_MODULE_1___default.a),
+/* harmony export */   "RenderFromTemplateContext": () => (/* reexport default from dynamic */ next_dist_client_components_render_from_template_context__WEBPACK_IMPORTED_MODULE_2___default.a),
+/* harmony export */   "__next_app_webpack_require__": () => (/* binding */ __next_app_webpack_require__),
+/* harmony export */   "pages": () => (/* binding */ pages),
+/* harmony export */   "renderToReadableStream": () => (/* reexport safe */ next_dist_compiled_react_server_dom_webpack_server_browser__WEBPACK_IMPORTED_MODULE_7__.renderToReadableStream),
+/* harmony export */   "requestAsyncStorage": () => (/* reexport safe */ next_dist_client_components_request_async_storage__WEBPACK_IMPORTED_MODULE_5__.requestAsyncStorage),
+/* harmony export */   "serverHooks": () => (/* reexport module object */ next_dist_client_components_hooks_server_context__WEBPACK_IMPORTED_MODULE_6__),
+/* harmony export */   "staticGenerationAsyncStorage": () => (/* reexport safe */ next_dist_client_components_static_generation_async_storage__WEBPACK_IMPORTED_MODULE_4__.staticGenerationAsyncStorage),
+/* harmony export */   "tree": () => (/* binding */ tree)
+/* harmony export */ });
+/* harmony import */ var next_dist_client_components_app_router__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(2315);
+/* harmony import */ var next_dist_client_components_app_router__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(next_dist_client_components_app_router__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_dist_client_components_layout_router__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(2333);
+/* harmony import */ var next_dist_client_components_layout_router__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_dist_client_components_layout_router__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var next_dist_client_components_render_from_template_context__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(2885);
+/* harmony import */ var next_dist_client_components_render_from_template_context__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(next_dist_client_components_render_from_template_context__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var next_dist_client_components_error_boundary__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(9505);
+/* harmony import */ var next_dist_client_components_error_boundary__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(next_dist_client_components_error_boundary__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var next_dist_client_components_static_generation_async_storage__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(683);
+/* harmony import */ var next_dist_client_components_static_generation_async_storage__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(next_dist_client_components_static_generation_async_storage__WEBPACK_IMPORTED_MODULE_4__);
+/* harmony import */ var next_dist_client_components_request_async_storage__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(3269);
+/* harmony import */ var next_dist_client_components_request_async_storage__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(next_dist_client_components_request_async_storage__WEBPACK_IMPORTED_MODULE_5__);
+/* harmony import */ var next_dist_client_components_hooks_server_context__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(5746);
+/* harmony import */ var next_dist_client_components_hooks_server_context__WEBPACK_IMPORTED_MODULE_6___default = /*#__PURE__*/__webpack_require__.n(next_dist_client_components_hooks_server_context__WEBPACK_IMPORTED_MODULE_6__);
+/* harmony import */ var next_dist_compiled_react_server_dom_webpack_server_browser__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(8208);
+/* harmony import */ var next_dist_compiled_react_server_dom_webpack_server_browser__WEBPACK_IMPORTED_MODULE_7___default = /*#__PURE__*/__webpack_require__.n(next_dist_compiled_react_server_dom_webpack_server_browser__WEBPACK_IMPORTED_MODULE_7__);
+
+    const tree = {
+        children: [
+        '',
+        {
+        children: ['', {}, {
+          page: [() => Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 8124)), "C:\\Users\\ghulam.ghaus\\Desktop\\temp\\NextJs-TODO-APP\\ghaus-todo-app\\app\\page.tsx"]}]
+      },
+        {
+          'layout': [() => Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 8514)), "C:\\Users\\ghulam.ghaus\\Desktop\\temp\\NextJs-TODO-APP\\ghaus-todo-app\\app\\layout.tsx"],
+'head': [() => Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 447)), "C:\\Users\\ghulam.ghaus\\Desktop\\temp\\NextJs-TODO-APP\\ghaus-todo-app\\app\\head.tsx"],
+        }
+      ]
+      }.children;
+    const pages = ["C:\\Users\\ghulam.ghaus\\Desktop\\temp\\NextJs-TODO-APP\\ghaus-todo-app\\app\\page.tsx"]
+
+    
+    
+    
+    
+
+    
+    
+
+    
+
+    
+    const __next_app_webpack_require__ = __webpack_require__
+  
+
+/***/ }),
+
+/***/ 2154:
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 5591))
+
+/***/ }),
+
+/***/ 8124:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ Home)
+});
+
+// EXTERNAL MODULE: ./node_modules/next/dist/compiled/react/jsx-runtime.js
+var jsx_runtime = __webpack_require__(8499);
+;// CONCATENATED MODULE: ./lib/task.ts
+// const BASE_URL = "https://localhost:3000";
+const BASE_URL = "https://jsonplaceholder.typicode.com/todos/";
+const taskApi = {
+    create: async (task)=>{
+        const res = await fetch(`${BASE_URL}/todos`, {
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            },
+            method: "POST",
+            body: JSON.stringify(task)
+        });
+        return res.json();
+    },
+    getAll: async ()=>{
+        const res = await fetch(`${BASE_URL}`);
+        return res.json();
+    },
+    update: async (task)=>{
+        const res = await fetch(`${BASE_URL}/todos/${task.id}`, {
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            },
+            method: "PATCH",
+            body: JSON.stringify(task)
+        });
+        return res.json();
+    },
+    delete: async (id)=>{
+        const res = await fetch(`${BASE_URL}/todos/${id}`, {
+            method: "DELETE"
+        });
+        return res.json();
+    }
+};
+
+// EXTERNAL MODULE: ./components/TaskItem.tsx
+var TaskItem = __webpack_require__(5496);
+var TaskItem_default = /*#__PURE__*/__webpack_require__.n(TaskItem);
+;// CONCATENATED MODULE: ./app/page.tsx
+// import { taskApi } from "@/lib/task";
+// export default async function Home() {
+//   const task = await taskApi.getAll();
+//   return <h1></h1>;
+// }
+
+
+
+
+async function Home() {
+    const tasks = await taskApi.getAll();
+    // console.log(tasks);
+    console.log(tasks.length);
+    return /*#__PURE__*/ jsx_runtime.jsx("div", {
+        className: "absolute inset-0 w-full h-max max-h-full overflow-auto rounded bg-black/10 dark:bg-white/5",
+        children: tasks.length > 0 ? /*#__PURE__*/ jsx_runtime.jsx("div", {
+            className: "p-4",
+            children: tasks.map((todo)=>/*#__PURE__*/ jsx_runtime.jsx((TaskItem_default()), {
+                    data: todo
+                }, todo.id))
+        }) : /*#__PURE__*/ jsx_runtime.jsx("div", {
+            className: "p-4 text-center text-gray-500 font-medium",
+            children: "No Tasks..."
+        })
+    });
+}
+
+
+/***/ }),
+
+/***/ 5496:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+/* __next_internal_client_entry_do_not_use__ */ const { createProxy  } = __webpack_require__(4353);
+module.exports = createProxy("C:\\Users\\ghulam.ghaus\\Desktop\\temp\\NextJs-TODO-APP\\ghaus-todo-app\\components\\TaskItem.tsx");
+
+
+/***/ }),
+
+/***/ 5591:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ components_TaskItem)
+});
+
+// EXTERNAL MODULE: external "next/dist/compiled/react/jsx-runtime"
+var jsx_runtime_ = __webpack_require__(6786);
+;// CONCATENATED MODULE: ./lib/task.ts
+// const BASE_URL = "https://localhost:3000";
+const BASE_URL = "https://jsonplaceholder.typicode.com/todos/";
+const taskApi = {
+    create: async (task)=>{
+        const res = await fetch(`${BASE_URL}/todos`, {
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            },
+            method: "POST",
+            body: JSON.stringify(task)
+        });
+        return res.json();
+    },
+    getAll: async ()=>{
+        const res = await fetch(`${BASE_URL}`);
+        return res.json();
+    },
+    update: async (task)=>{
+        const res = await fetch(`${BASE_URL}/todos/${task.id}`, {
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            },
+            method: "PATCH",
+            body: JSON.stringify(task)
+        });
+        return res.json();
+    },
+    delete: async (id)=>{
+        const res = await fetch(`${BASE_URL}/todos/${id}`, {
+            method: "DELETE"
+        });
+        return res.json();
+    }
+};
+
+// EXTERNAL MODULE: ./node_modules/next/navigation.js
+var navigation = __webpack_require__(9483);
+// EXTERNAL MODULE: external "next/dist/compiled/react"
+var react_ = __webpack_require__(8038);
+;// CONCATENATED MODULE: ./components/AnimatedCheck.tsx
+// "use-";
+
+
+const AnimatedChek = ({ checked , toggle  })=>{
+    const totalLength = checked ? 0 : 40;
+    return /*#__PURE__*/ jsx_runtime_.jsx("div", {
+        className: "grid place-items-center w-8 h-8 rounded bg-black/10 dark:bg-white/10",
+        onClick: toggle,
+        children: /*#__PURE__*/ jsx_runtime_.jsx("svg", {
+            width: 23,
+            height: 23,
+            viewBox: "0 0 23 23",
+            fill: "none",
+            children: /*#__PURE__*/ jsx_runtime_.jsx("path", {
+                className: "transition-all duration-300",
+                strokeDashoffset: totalLength,
+                strokeDasharray: totalLength,
+                stroke: "#0b9219",
+                strokeWidth: 3,
+                strokeLinecap: "round",
+                d: "M21.5 2C17 6 9.8 15.3091 9 22C8.33333 19.2121 6 12.8 2 9"
+            })
+        })
+    });
+};
+/* harmony default export */ const AnimatedCheck = (AnimatedChek);
+
+;// CONCATENATED MODULE: ./components/TaskItem.tsx
+
+
+
+
+
+const TaskItem = ({ data  })=>{
+    const router = navigation.useRouter;
+    const [isEdited, setIsEdited] = (0,react_.useState)(false);
+    const [loading, setLoading] = (0,react_.useState)(false);
+    const handleUpdateTask = async (value)=>{
+        setLoading(true);
+        await taskApi.update({
+            completed: value
+        });
+        setIsEdited(value);
+        setLoading(false);
+    };
+    return /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+        className: "flex bg-white dark:bg-[#1c1c1c] p-2mb-2 last:mb-0 rounded cursor-pointer",
+        children: [
+            /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+                className: "basis-8 mr-2 ",
+                children: [
+                    /*#__PURE__*/ jsx_runtime_.jsx(AnimatedCheck, {
+                        checked: isEdited,
+                        toggle: ()=>handleUpdateTask(!isEdited)
+                    }),
+                    "."
+                ]
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx("div", {
+                className: "flex flex-1",
+                children: "Input"
+            })
+        ]
+    });
+};
+/* harmony default export */ const components_TaskItem = (TaskItem);
+
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = __webpack_require__.X(0, [430,295], () => (__webpack_exec__(5640)));
+module.exports = __webpack_exports__;
+
+})();
